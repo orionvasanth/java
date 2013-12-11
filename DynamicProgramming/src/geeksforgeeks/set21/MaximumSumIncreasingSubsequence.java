4937badc55b2b26106d4e35a1655a3fe3b31ae8f
Vasanth Kumar Vadivelu <orionvasanth@gmail.com>
2013-12-11 15:12:48 +0530

    Max sum increasing sub-seq

--- DynamicProgramming/src/geeksforgeeks/set21/MaximumSumIncreasingSubsequence.java
+++ DynamicProgramming/src/geeksforgeeks/set21/MaximumSumIncreasingSubsequence.java
@@ -1,13 +1,48 @@
 package geeksforgeeks.set21;
 
+import java.util.Arrays;
+
 public class MaximumSumIncreasingSubsequence {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] input = { 1, 101, 2, 3, 100, 4, 5 };
+		System.out.println(new MaximumSumIncreasingSubsequence()
+				.findMaximumSumIncreasingSubsequence(input));
 
+	}
+
+	private int[] dp;
+
+	public int findMaximumSumIncreasingSubsequence(int[] input) {
+		dp = new int[input.length];
+		Arrays.fill(dp, 0);
+		dp[0] = input[0];
+
+		findMaxSum(input);
+		int max = 0;
+		for (int i = 0; i < dp.length; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		return max;
+	}
+
+	private void findMaxSum(int[] input) {
+		for (int i = 1; i < input.length; i++) {
+			dp[i] = findLast(input, i) + input[i];
+		}
+	}
+
+	private int findLast(int[] input, int index) {
+		int max = 0;
+		for (int i = 0; i < index; i++) {
+			if (input[i] < input[index]) {
+				max = Math.max(max, dp[i]);
+			}
+		}
+		return max;
 	}
 
 }
